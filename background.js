58d14af8e5775d9fe64faf58f39a2571f097ac80
dimapopov <dima1popov@ukr.net>
2016-02-09 12:37:26 +0300

    0.7.0

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ $.get(chrome.extension.getURL("/jquery-2.2.0.js"),
 		document.getElementsByTagName("head")[0].appendChild(script);
 	}
 );
-$.get(chrome.extension.getURL('/orna-0.6.0.js'), 
+$.get(chrome.extension.getURL('/orna-0.7.0.js'), 
 	function(data) {
 		var script = document.createElement("script");
 		script.setAttribute("type", "text/javascript");
